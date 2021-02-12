d7325e9546ec1da9ed95852f6bb694c6d6d10a2b
mclmnop <m_charlebois@hotmail.com>
2021-02-11 19:44:56 -0500

    with fly over times

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -6,5 +7,22 @@ fetchMyIP((error, ip) => {
     return;
   }
   console.log('It worked! Here is my IP: ', ip);
+});
+
+fetchCoordsByIP('70.81.220.168', (error, data) => {
+  if (error) {
+    console.log('It didn\'t work, ', error);
+    return;
+  }
+  console.log('It worked! Here are my coordinates: ', data);
+});
+
+//fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, data) => {
+fetchISSFlyOverTimes({ latitude: '49', longitude: '-123.13000' }, (error, data) => {
+  if (error) {
+    console.log('It didn\'t work, ', error);
+    return;
+  }
+  console.log('It worked! Here are the fly over times: ', data);
 });
 
--- iss.js
+++ iss.js
@@ -6,8 +6,9 @@ const request = require('request');
 *   - An error, if any (nullable)
 *   - The IP address as a string (null if error). Example: "162.245.144.188"
 */
+
 const fetchMyIP = (callback) => {
-  request.get('https://api.ipify.org/poil', (error, response, body) => {
+  request.get('https://api.ipify.org/?format=json', (error, response, body) => {
     if (error) {
       return callback(error, null);
     }
@@ -16,11 +17,51 @@ const fetchMyIP = (callback) => {
       // callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null); << from compass
       return callback(msg, null);
     }
-
     const data = JSON.parse(body);
     callback(null, data.ip);
   });
 };
 
+const fetchCoordsByIP = (ip, callback) => {
+  request.get(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      // callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null); << from compass
+      return callback(msg, null);
+    }
+    const data = JSON.parse(body);
+    //console.log(data)
+    const coords = {
+      latitude: data.latitude,
+      longitude: data.longitude
+    };
+    return callback(null, coords);
+  });
+};
 
-module.exports = { fetchMyIP };
+//http://api.open-notify.org/iss-pass.json?lat=LAT&lon=LON
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const LON = Math.round(coords.longitude);
+  const LAT = Math.round(coords.latitude);
+  //console.log(LAT, LON)
+  request.get(`http://api.open-notify.org/iss-pass.json?lat=${LAT}&lon=${LON}`, (error, response, body) => {
+    //http://api.open-notify.org/iss-pass.json?lat=49&lon=-123
+    if (error) {
+      return callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      // callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null); << from compass
+      return callback(msg, null);
+    }
+    const data = JSON.parse(body);
+    return callback(null, data.response);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
+// module.exports = { fetchCoordsByIP };
